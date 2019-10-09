cea75a154e4b1561ff8e23db1c3ced1c77c72eeb
GPaddle <keller.guillaume68@gmail.com>
2019-10-09 11:15:05 +0200

    Change the P5JS way to work to a simple JS program

--- src/client/app.js
+++ src/client/app.js
@@ -13,56 +13,121 @@ const state = {
     lastX: -1,
     lastY: -1,
     ui: {
+        canvas: undefined,
         colorPicker: undefined,
         resetButton: undefined,
         fillButton: undefined,
-    }
+    },
+    color: "#ffff00"
+
 };
 
-function setup() {
+function initialise() {
 
-    state.cellSize = displayWidth - props.canvasMargin * 2; // remove margins
+    //    let canvas = document.getElementById('mycanvas').width;
+    //    let height = canvas.height;
+
+
+    state.cellSize = document.getElementById('canvas1').width - props.canvasMargin * 2; // remove margins
     state.cellSize -= (props.borderWidth * (props.panelWidth + 1)); // remove borders ;)
     state.cellSize /= props.panelWidth; // divide by number of cells horizontally
-    state.cellSize = int(state.cellSize);
+    state.cellSize = parseInt(state.cellSize);
 
-    state.canvasWidth = state.cellSize * props.panelWidth + (props.panelWidth - 1) * props.borderWidth;
-    state.canvasHeight = state.cellSize * props.panelHeight + (props.panelHeight - 1) * props.borderWidth;
+    //    state.canvasWidth = state.cellSize * props.panelWidth + (props.panelWidth - 1) * props.borderWidth;
+    //    state.canvasHeight = state.cellSize * props.panelHeight + (props.panelHeight - 1) * props.borderWidth;
 
-    createCanvas(state.canvasWidth, state.canvasHeight);
-    background('grey');
-    state.ui.colorPicker = createColorPicker('#f00');
+    let canvas = document.getElementById('canvas1');
+    state.canvasWidth = canvas.width;
+    state.canvasHeight = canvas.height;
 
-    state.ui.resetButton = createButton('reset');
-    state.ui.resetButton.mousePressed(resetPannel);
 
-    state.ui.fillButton = createButton('fill');
-    state.ui.fillButton.mousePressed(function() {
-        let color = state.ui.colorPicker.color();
-        fillPanel(color);
-    });
+    state.ui.canvas = canvas;
 
     for (let x = 0; x < props.panelWidth; x++) {
         state.panelData[x] = [];
         for (let y = 0; y < props.panelHeight; y++) {
-            state.panelData[x][y] = color(0, 0, 0);
+            state.panelData[x][y] = "#000000";
         }
-
     }
+    state.ui.colorPicker = document.getElementById('colorPicker');
+    document.getElementById('colorPicker').addEventListener("change", watchColorPicker, false);
+
+    state.ui.fillButton = document.getElementById("fill");
+    state.ui.fillButton.addEventListener("click", function() {
+        fill(state.color);
+    });
+    state.ui.resetButton = document.getElementById("reset");
+    state.ui.resetButton.addEventListener("click", function() {
+        fill("#000000");
+    });
+
+    state.ui.canvas.addEventListener("mousedown", onMouseDown, false);
+
+    let wsUrl = window.location.protocol === "https:" ? "wss://" : "ws://";
+    wsUrl += window.location.host + window.location.pathname;
+    wsUrl += "draw";
+
+    console.log("WebSocket to", wsUrl);
+    const webSocket = new WebSocket(wsUrl);
+
+    draw();
+
+
 }
 
-
-function draw() {
-    strokeWeight(0);
+function fill(color) {
     for (let x = 0; x < props.panelWidth; x++) {
         for (let y = 0; y < props.panelHeight; y++) {
-            fill(state.panelData[x][y]);
-            rect((state.cellSize + props.borderWidth) * x,
-                (state.cellSize + props.borderWidth) * y,
-                state.cellSize,
-                state.cellSize);
+            if (state.panelData[x][y] != color) {
+                state.panelData[x][y] = color;
+                let r = parseInt(color.substring(1, 3), 16);
+                let g = parseInt(color.substring(3, 5), 16);
+                let b = parseInt(color.substring(5, 7), 16);
+
+                sendData(x, y, r, g, b);
+            }
         }
     }
+    draw();
+}
+
+function watchColorPicker(event) {
+    state.color = event.target.value;
+}
+
+function onMouseDown(event) {
+
+
+    function onMouseMove(event) {
+        var canvas_x = event.pageX;
+        var canvas_y = event.pageY;
+
+
+        if (canvas_x >= 0 && canvas_x <= state.canvasWidth && canvas_y >= 0 && canvas_y <= state.canvasHeight) {
+
+            let targetX = parseInt(canvas_x / (state.cellSize + 2 * props.borderWidth));
+            let targetY = parseInt(canvas_y / (state.cellSize + 2 * props.borderWidth));
+            if (state.color != state.panelData[targetX][targetY]) {
+                state.panelData[targetX][targetY] = state.color;
+                //console.log(state.color);
+                let r = parseInt(state.color.substring(1, 3), 16);
+                let g = parseInt(state.color.substring(3, 5), 16);
+                let b = parseInt(state.color.substring(5, 7), 16);
+
+                sendData(targetX, targetY, r, g, b);
+
+
+            }
+            draw();
+        }
+    }
+
+    function onMouseUp(event) {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+    }
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
 }
 
 
@@ -70,61 +135,29 @@ function sendData(x, y, r, g, b) {
     if (webSocket.readyState === webSocket.OPEN) {
         let color = state.panelData[x][y];
 
-        let r1 = color.levels[0];
-        let g1 = color.levels[1];
-        let b1 = color.levels[2];
+        let r1 = color.r;
+        let g1 = color.g;
+        let b1 = color.b;
         if (r != r1 || g != g1 || b != b1) {
             webSocket.send([x, y, r, g, b]);
         }
     }
 }
 
-function resetPannel() {
-    fillPanel(color(0, 0, 0));
-}
+function draw() {
 
-
-function fillPanel(col) {
+    state.ui.canvas = document.getElementById("canvas1").getContext("2d");
     for (let x = 0; x < props.panelWidth; x++) {
         for (let y = 0; y < props.panelHeight; y++) {
-            sendData(x, y, col.levels[0], col.levels[1], col.levels[2]);
-            state.panelData[x][y] = col;
+            state.ui.canvas.fillStyle = state.panelData[x][y];
+            state.ui.canvas.fillRect((state.cellSize + props.borderWidth) * x,
+                (state.cellSize + props.borderWidth) * y,
+                state.cellSize,
+                state.cellSize);
         }
     }
 }
 
-function mouseDragged() {
-    if (mouseX >= 0 &&
-        mouseX <= (state.cellSize + props.borderWidth) * props.panelWidth &&
-        mouseY >= 0 && mouseY <= (state.cellSize + props.borderWidth) * props.panelHeight) {
-        let x = mouseX;
-        let y = mouseY;
-        let color = state.ui.colorPicker.color();
-        // TODO: this is where we push the change to the server
-        let targetX = int(x / (state.cellSize + props.borderWidth));
-        let targetY = int(y / (state.cellSize + props.borderWidth));
-
-        let r = color.levels[0];
-        let g = color.levels[1];
-        let b = color.levels[2];
-
-        var colorOld = state.panelData[targetX][targetY];
-        let r1 = red(colorOld);
-        let g1 = green(colorOld);
-        let b1 = blue(colorOld);
-
-        //If the color has not changed, don't update it
-
-        if ((targetX != state.lastX || targetY != state.lastY) &&
-            (r != r1 || g != g1 || b != b1)) {
-
-            sendData(targetX, targetY, r, g, b);
-            state.panelData[targetX][targetY] = color;
-        }
-        state.lastX = targetX;
-        state.lastY = targetY;
-    }
-}
 
 let wsUrl = window.location.protocol === "https:" ? "wss://" : "ws://";
 wsUrl += window.location.host + window.location.pathname;
