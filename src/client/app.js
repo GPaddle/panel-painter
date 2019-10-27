d51185ef04b3b2bf7fbc513eea5579486c55d21a
GPaddle <keller.guillaume68@gmail.com>
2019-10-27 23:46:04 +0100

    Add eraser/hue color picker
    Add a function to erase (exchange color and a tmp color #000000)
    Add the function to pick a color in a hue slider + convert it to rgb

--- src/client/app.js
+++ src/client/app.js
@@ -13,11 +13,16 @@ const state = {
     lastX: -1,
     lastY: -1,
     color: "#ffff00",
+    color2: "#000000",
+    draw: true,
     ui: {
         canvas: undefined,
         colorPicker: undefined,
         resetButton: undefined,
         fillButton: undefined,
+        drawEraseButton: undefined,
+        hueSlider: undefined,
+        HueSliderText: undefined,
     }
 
 };
@@ -44,8 +49,18 @@ function initialise() {
             state.panelData[x][y] = "#000000";
         }
     }
+
+    state.ui.hueSlider = document.getElementById("HueSlider");
+    state.ui.HueSliderText = document.getElementById("HueSliderText");
+    state.ui.HueSliderText.innerText = "000";
+
+    state.ui.drawEraseButton = document.getElementById("drawEraseButton");
+    state.ui.drawEraseButton.addEventListener("click", changeDrawErase);
+
+    document.getElementById("eraseLogo").style.display = "contents";
+
     state.ui.colorPicker = document.getElementById("colorPicker");
-    document.getElementById("colorPicker").addEventListener("change", watchColorPicker, false);
+    state.ui.colorPicker.addEventListener("change", watchColorPicker, false);
 
     state.ui.fillButton = document.getElementById("fill");
     state.ui.fillButton.addEventListener("click", function () {
@@ -74,6 +89,41 @@ function computeCellSize() {
     state.cellSize -= (props.borderWidth * (props.panelWidth + 1));
     state.cellSize /= props.panelWidth;
     state.cellSize = parseInt(state.cellSize);
+}
+
+function changeDrawErase() {
+
+    state.draw = !state.draw;
+
+    //drawing mode
+    if (state.draw) {
+
+        console.log(state.color + " c1|c2 " + state.color2);
+        let c = state.color;
+        console.log(state.color + " c1|c2 " + state.color2);
+        state.color = state.color2;
+        console.log(state.color + " c1|c2 " + state.color2);
+        state.color2 = "#000000";
+        console.log(state.color + " c1|c2 " + state.color2);
+
+
+        document.getElementById("eraseLogo").style.display = "contents";
+        document.getElementById("drawLogo").style.display = "none";
+
+        //erasing mode
+    } else {
+
+        console.log(state.color + " c1|c2 " + state.color2);
+        state.color2 = state.color;
+        console.log(state.color + " c1|c2 " + state.color2);
+        state.color = "#000000";
+        console.log(state.color + " c1|c2 " + state.color2);
+
+        document.getElementById("drawLogo").style.display = "contents";
+        document.getElementById("eraseLogo").style.display = "none";
+
+    }
+
 }
 
 function reComputeSize() {
@@ -111,6 +161,67 @@ function fill(color) {
 function watchColorPicker(event) {
     state.color = event.target.value;
     document.getElementById("fillBucket").style.color = state.color;
+}
+
+function setColor(e) {
+    let value = state.ui.hueSlider.value;
+    console.log(value);
+    state.ui.HueSliderText.innerText = value;
+    let color = hslToRgb(value / 360, 1.00, 0.50);
+
+    let r = color[0].toString(16);
+    let g = color[1].toString(16);
+    let b = color[2].toString(16);
+
+    if (r.length == 1) {
+        r = "0" + r;
+    }
+    if (g.length == 1) {
+        g = "0" + g;
+    }
+    if (b.length == 1) {
+        b = "0" + b;
+    }
+
+
+    state.color = "#" + r + g + b;
+    console.log(state.color);
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   {number}  h       The hue
+ * @param   {number}  s       The saturation
+ * @param   {number}  l       The lightness
+ * @return  {Array}           The RGB representation
+ */
+function hslToRgb(h, s, l) {
+    var r, g, b;
+
+    if (s == 0) {
+        r = g = b = l; // achromatic
+    } else {
+        var hue2rgb = function hue2rgb(p, q, t) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1 / 6) return p + (q - p) * 6 * t;
+            if (t < 1 / 2) return q;
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+            return p;
+        };
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1 / 3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1 / 3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
 
 function touchStart(e) {
@@ -159,16 +270,23 @@ function onMouseDown(event) {
 }
 
 function sendData(x, y, r, g, b) {
-    if (webSocket.readyState === webSocket.OPEN) {
-        let color = state.panelData[x][y];
+    try {
 
-        let r1 = color.r;
-        let g1 = color.g;
-        let b1 = color.b;
-        if (r != r1 || g != g1 || b != b1) {
-            webSocket.send(JSON.stringify([x, y, r, g, b]));
+        if (webSocket.readyState === webSocket.OPEN) {
+            let color = state.panelData[x][y];
+
+            let r1 = color.r;
+            let g1 = color.g;
+            let b1 = color.b;
+            if (r != r1 || g != g1 || b != b1) {
+                webSocket.send(JSON.stringify([x, y, r, g, b]));
+            }
         }
+
+    } catch (error) {
+        console.log("WebSocket problem");
     }
+
 }
 
 function draw() {
@@ -211,7 +329,6 @@ function processingCoords(x, y) {
     }
 }
 
-
 function readURL(input) {
     if (input.files && input.files[0]) {
         let reader = new FileReader();
@@ -224,16 +341,11 @@ function readURL(input) {
         };
         reader.readAsDataURL(input.files[0]);
 
-
         let canvas = document.getElementById('canvasTmp');
         let ctx = canvas.getContext('2d');
-        //    alert("1");
 
         let image = document.getElementById("source");
 
-
-
-        //    alert("2");
         image.onload = function () {
 
             let w = image.width;
@@ -252,7 +364,6 @@ function readURL(input) {
             canvas2.height = 10 * h;
 
             //alert(canvas2.width);
-
 
             ctx2.fillStyle = "white";
             ctx2.fillRect(0, 0, canvas2.width, canvas2.height);
@@ -278,9 +389,7 @@ function readURL(input) {
                         let r = data.data[0];
                         let g = data.data[1];
                         let b = data.data[2];
-
                         sendData(x, y, r, g, b);
-
                     }
                     draw();
                 }
@@ -288,7 +397,6 @@ function readURL(input) {
         };
     }
 }
-
 
 let wsUrl = window.location.protocol === "https:" ? "wss://" : "ws://";
 wsUrl += window.location.host;
